17255b0674e00098e155d52d5c4b25be82317765
Peter Ariyibi <domnigg2020@gmail.com>
2024-11-15 00:17:51 +0000

    Added file

--- lesson6.c
+++ lesson6.c
@@ -1,80 +1,64 @@
-/*
-
-Modify the get_first() function of Listing 8.8 so that it returns the first non-
-whitespace character encountered. Test it in a simple program.
-
-*/
+#include <stdio.h>
 #include <ctype.h>  // For isspace()
 
-#include <stdio.h>
 char get_choice(void);
 char get_first(void);
 int get_int(void);
 void count(void);
-int main(void)
-{
-int choice;
-void count(void);
-while ( (choice = get_choice()) != 'q')
-{
-switch (choice)
-{
-case 'a' : printf("Buy low, sell high.\n");
-break;
-case 'b' : putchar('\a'); /* ANSI */
-break;
-case 'c' : count();
-break;
-default : printf("Program error!\n");
-break;
-}
-}
-printf("Bye.\n");
 
-return 0;
+int main(void) {
+    int choice;
 
+    while ((choice = get_choice()) != 'q') {
+        switch (choice) {
+        case 'a': printf("Buy low, sell high.\n");
+            break;
+        case 'b': putchar('\a'); /* ANSI */
+            break;
+        case 'c': count();
+            break;
+        default: printf("Program error!\n");
+            break;
+        }
+    }
+    printf("Bye.\n");
+
+    return 0;
 }
 
-void count(void)
-{
-int n,i;
-printf("Count how far? Enter an integer:\n");
-n = get_int();
-for (i = 1; i <= n; i++)
-printf("%d\n", i);
-while ( getchar() != '\n')
-continue;
+void count(void) {
+    int n, i;
+    printf("Count how far? Enter an integer:\n");
+    n = get_int();
+    for (i = 1; i <= n; i++)
+        printf("%d\n", i);
+    while (getchar() != '\n')
+        continue;
 }
 
-char get_choice(void)
-{
-int ch;
-printf("Enter the letter of your choice:\n");
-printf("a. advice b. bell\n");
-printf("c. count q. quit\n");
-ch = get_first();
-while ( (ch < 'a' || ch > 'c') && ch != 'q')
-{
-printf("Please respond with a, b, c, or q.\n");
-ch = get_first();
+char get_choice(void) {
+    int ch;
+    printf("Enter the letter of your choice:\n");
+    printf("a. advice b. bell\n");
+    printf("c. count q. quit\n");
+    ch = get_first();
+    while ((ch < 'a' || ch > 'c') && ch != 'q') {
+        printf("Please respond with a, b, c, or q.\n");
+        ch = get_first();
+    }
+    return ch;
 }
-return ch;
-}
-
 
 char get_first(void) {
     int ch;
     
     // Skip whitespace characters
     while (isspace(ch = getchar())) {
-        // Nothing to do, just skip whitespaces
+        // Just skip whitespace characters
     }
-    
+
     return ch;  // Return the first non-whitespace character
-
 }
-
-
 
 int get_int(void) {
     int input;
